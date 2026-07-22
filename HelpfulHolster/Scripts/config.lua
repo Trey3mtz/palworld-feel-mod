8563f43e094e6e818ce0bf7410e0d8a525bd42f5
Enrique Martinez <112442527+Trey3mtz@users.noreply.github.com>
2026-07-22 10:57:59 -0500

    Update config.lua

--- HelpfulHolster/Scripts/config.lua
+++ HelpfulHolster/Scripts/config.lua
@@ -3,7 +3,7 @@ return
     KeyboardBinding = "H",
     ControllerBinding = "Gamepad_FaceButton_Top",
     HoldToHolster = true,
-    HoldTimeSeconds = 0.5
+    HoldTimeSeconds = 0.6
 }
 
 -- Binding names come from Unreal Engine's EKey documentation
